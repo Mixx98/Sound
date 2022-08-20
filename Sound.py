34602ce396d020d51f7b1fa5d2321a3a0beef129
Hyunsue Yun <yyhs6972@naver.com>
2022-08-20 23:39:23 +0900

    test2

--- Sound.py
+++ Sound.py
@@ -1,25 +1,19 @@
-import librosa.display, librosa
-import IPython.display
-import warnings
-import numpy as np
 import matplotlib.pyplot as plt
+import librosa.display, librosa
+import numpy as np
 
-warnings.simplefilter("ignore")
+def get_stft(sample_sounds):
+    return librosa.stft(sample_sounds)
 
-def sin_wave(amp, freq, time):
-    return amp * np.sin(2*np.pi*freq*time)
+def get_chroma(sample_sounds, sr):
+    return librosa.feature.chroma_stft(S=np.abs(get_stft(sample_sounds)), 
+                                       sr=sr)
 
-sr = 22*1000.
-ts = 1/sr
+def draw_chroma(sample_sounds, sr):
+    plt.figure(figsize=(12,6))
+    librosa.display.specshow(get_chroma(sample_sounds, sr), 
+                             y_axis='chroma', x_axis='time')
+    plt.grid(); plt.show()
 
-time = np.arange(0, 1, ts)
-DO = sin_wave(1, 261.6256, time)
-MI = sin_wave(1, 329.6276, time)
-SOL = sin_wave(1, 391.9954, time)
-
-plt.figure(figsize=(12,5))
-plt.plot(time, DO, label="DO", color='red')
-plt.plot(time, MI, label="MI", color='blue')
-plt.plot(time, SOL, label="SOL", color='green')
-plt.xlim((0, 0.01))
-plt.legend(); plt.grid(); plt.show()
+piano, sr=librosa.core.load('piano.wav')
+draw_chroma(piano,sr)
